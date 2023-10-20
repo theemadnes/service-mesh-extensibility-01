d0ed3b16bb91dde56ef45a540db38fa0790c7a14
theemadnes <alex.mattson@gmail.com>
2023-10-20 03:39:45 +0000

    working for plaintext

--- hello-envoy/main.go
+++ hello-envoy/main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 )
 
+// not used any more
 type Message struct {
 	Message string `json:"message"`
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		m := Message{Message: "Hello, Envoy!"}
-		b, err := json.Marshal(m)
+		//m := Message{Message: "Hello, Envoy!"}
+		m := "Hello, Envoy!"
+		/*b, err := json.Marshal(m)
 		if err != nil {
 			log.Fatal(err)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(b)
+		}*/
+		//w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Type", "text/plain")
+		//w.Write(b)
+		w.Write(([]byte)(m))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
